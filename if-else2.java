79e0a84a7451042e6df9c2f39e7726bb43df5054
Lavanya-T-S <115984634+Lavanya-T-S@users.noreply.github.com>
2023-03-29 20:42:10 +0530

    Add files via upload
    if-else statement execution in java

---
+++ if-else2.java
@@ -0,0 +1,15 @@
+public class hlo {
+    public static void main(String args[]){
+        boolean i=true;
+        if(i=true)
+        {
+           System.out.println("hello");
+       }
+       else
+       {
+        System.out.println("hii ");
+        }
+    }
+}
+    
+
